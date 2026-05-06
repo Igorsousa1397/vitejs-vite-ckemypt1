2bc9a6630333ce67f38fa563c552d722cbc0910d
Igor Oliveira de sousa <111364118+Igorsousa1397@users.noreply.github.com>
2026-05-06 17:01:07 +0000

    feat: versão e limpeza de cache no service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,27 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
+const VERSION = 'v1.0.2';
+
+// Força atualização ao instalar nova versão
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 firebase.initializeApp({
   apiKey: "AIzaSyCC5a-yhc0myEnlhcs5pgCXOSx1h6b5CmU",
   authDomain: "servos-peniel.firebaseapp.com",
@@ -11,7 +32,6 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
   const { title, body } = payload.notification;
   self.registration.showNotification(title, {
