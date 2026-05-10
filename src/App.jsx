ab67b31bd7885cdbf3c600310452af669f059856
Igor Oliveira de sousa <111364118+Igorsousa1397@users.noreply.github.com>
2026-05-10 15:01:49 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3446,7 +3446,7 @@ export default function App() {
           )}
 
           {/* Centro */}
-          <div style={{ flex: 1, display: "flex", justifyContent: "center", paddingLeft: 130 }}>
+          <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
             {pg === "smins" ? (
               <img src="/IMG_2409.PNG" alt="Fonte" style={{ height: 44, mixBlendMode: "screen", opacity: 0.85 }} />
             ) : (
