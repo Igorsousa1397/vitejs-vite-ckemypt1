72374c124ce8dbae150af3ed08c8d20ec80cd86b
Igor sousa <igorsousa@MacBook-Air-de-Igor.local>
2026-05-13 14:34:28 -0300

    fix: remove o x da tela saude

--- src/App.jsx
+++ src/App.jsx
@@ -4315,18 +4315,6 @@ export default function App() {
             ph="Nome do encontrista..."
           />
         )}
-        {edit && (
-          <span
-            onClick={() => setSau(sau.filter((x) => x.id !== s.id))}
-            style={{
-              color: "rgba(255,59,48,.4)",
-              cursor: "pointer",
-              fontSize: 16,
-              marginLeft: 10,
-            }}
-          >
-          </span>
-        )}
         {pg === "louça" && (
           <LouçaV
             louça={louça}
